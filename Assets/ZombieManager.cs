4dc550f5be1f286a9119c3c34cc9feaac37061d7
Jens Iggström <jens.iggstrom@gmail.com>
2018-04-13 20:46:45 +0200

    zomibie tester

---
+++ Assets/ZombieManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieManager : MonoBehaviour
+{
+
+    public GameManager GameManager;
+
+    private Vector3 _lastFramePosition;
+    private Animator _anim;
+
+    void Start()
+    {
+        var ma = this.GetComponent<NavMeshAgent>();
+        _anim = this.GetComponent<Animator>();
+        _anim.Play("idle");
+        _lastFramePosition = transform.position;
+
+        ma.SetDestination(GameManager.Player.transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentFramePosition = transform.position;
+        float distance = Vector3.Distance(_lastFramePosition, currentFramePosition);
+
+        _lastFramePosition = currentFramePosition;
+        float currentSpeed = Mathf.Abs(distance) / Time.deltaTime;
+
+        // Switch between idle and walk
+        if (currentSpeed > 0.1)
+            _anim.Play("walk");
+        else
+            _anim.Play("idle");
+
+    }
+}
