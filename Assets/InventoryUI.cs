58f3390f26d3e8949c65afec672bb5cea62f669d
Jens Iggström <jens.iggstrom@yourvoice.se>
2018-06-21 15:58:22 +0200

    .

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -8,25 +8,41 @@ public class InventoryUI : MonoBehaviour {
     private GameManager gm;
     public GameObject Slots;
 
-    private InventorySlot[] slotlist;
+    private InventorySlot[] slotList;
 	// Use this for initialization
 	void Start () {
         gm = FindObjectOfType<GameManager>();
-        var slotList = Slots.GetComponentsInChildren<InventorySlot>();
+        slotList = Slots.GetComponentsInChildren<InventorySlot>();
         var slotindex = 0;
         foreach(Loot l in gm.inventory)
         {
             var slot = slotList[slotindex];
             slot.Image.sprite = l.InventoryImage;
+            slot.Image.enabled = true;
             slot.Removebutton.onClick.AddListener(()=> RemoveInventoryItem(slotindex, l));
+            slot.Removebutton.interactable = true;
         }
     }
-
+    void OnEnable()
+    {
+        var slotindex = 0;
+        foreach (Loot l in gm.inventory)
+        {
+            var slot = slotList[slotindex];
+            slot.Image.sprite = l.InventoryImage;
+            slot.Image.enabled = true;
+            slot.Removebutton.onClick.AddListener(() => RemoveInventoryItem(slotindex, l));
+            slot.Removebutton.interactable = true;
+        }
+    }
     private void RemoveInventoryItem(int slotindex, Loot l)
     {
         gm.RemoveInventoryItem(l);
-        slotlist[slotindex].Image.sprite = null;
-        slotlist[slotindex].Removebutton.onClick = null;
+        var slot = slotList[slotindex];
+        slot.Image.sprite = null;
+        slot.Image.enabled = false;
+        slot.Removebutton.onClick = null;
+        slot.Removebutton.interactable = false;
     }
 
     // Update is called once per frame
